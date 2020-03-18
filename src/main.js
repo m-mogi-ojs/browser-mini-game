74abc77813c8e5c10f50c2838f21290ad4926266
m-mogi-ojs <m.mogi.ojs@gmail.com>
2020-03-18 21:49:08 +0900

    ver1完成

--- src/main.js
+++ src/main.js
@@ -4,12 +4,14 @@ var cherryImage
 var risImage
 var counter
 var playTime = 0
+var score = 0
 var player
 var objects = {}
 
-const FRAME_PER_SEC = 60
-const IMAGE_SIZE = 32
-const PLAYER_VELOCITY = 5
+const FRAME_PER_SEC = 60 //メインフレームのFPS
+const IMAGE_SIZE = 32 // 使用画像の描画サイズ
+const PLAYER_VELOCITY = 5 // プレイヤー移動速度
+const SCORE_RISE_POINT = 100 // スコアの上げ幅
 
 class Vector {
   x = 0
@@ -29,7 +31,8 @@ class GameObject {
     this.pos = posVec
     this.vel = velVec
     this.imageObj = imageObj
-    this.id = new Date().getTime().toString(16)  + Math.floor(1000*Math.random()).toString(16)
+    //this.id = Math.floor(0xffffffff*Math.random()).toString(16)
+    this.id = new Date().getTime().toString(16)
     objects[this.id] = this
   }
   destroy() {
@@ -38,11 +41,13 @@ class GameObject {
   move() {
     this.pos.x += this.vel.x
     this.pos.y += this.vel.y
+    if (this.pos.y > canvas.height - IMAGE_SIZE*1.5) {
+      this.destroy()
+    }
   }
 }
 
 window.onload = function() {
-  console.log("onload end.")
   // 初期化処理
   initialize()
   // メインフレームを設定
@@ -55,12 +60,13 @@ var initialize = function() {
   context = canvas.getContext('2d')
   context.font = "12px Arial"
   cherryImage = new Image()
-  cherryImage.src = "./cherry.png"
+  cherryImage.src = "./banana.png"
   risImage = new Image()
-  risImage.src = "./ris.png"
+  risImage.src = "./monkey.png"
   player = new GameObject(
     new Vector(canvas.width / 2 - IMAGE_SIZE / 2, canvas.height - IMAGE_SIZE*1.5),
-    new Vector(0, 0), risImage)
+    new Vector(0, 0),
+    risImage)
   // プレイヤー操作を設定
   // メインフレームに合わせて操作を受け付けるべきだが今は簡単さを重視
   document.addEventListener('keydown', function(e) {
@@ -76,25 +82,27 @@ var initialize = function() {
 }
 
 var main = function() {
-  createDropObject()
   checkCollisionDropObject()
   Object.keys(objects).forEach(key => {
     objects[key].move()
   })
+  createDropObject()
   playTime += 1
   draw()
+  debugConsole()
 }
 
 var draw = function() {
   context.clearRect(0, 0, canvas.width, canvas.height)
   context.fillText("プレイ時間：" + Math.floor(playTime / FRAME_PER_SEC).toString(), 5, 5 + 12)
+  context.fillText("スコア：" + score, canvas.width - 80 - 5, 5 + 12)
   Object.keys(objects).reverse().forEach(key => {
     context.drawImage(objects[key].imageObj, objects[key].pos.x, objects[key].pos.y, IMAGE_SIZE, IMAGE_SIZE)
   });
 }
 
 var createDropObject = function() {
-  if (playTime / 30 % 2 === 0) {
+  if (playTime / 20 % 2 === 0) {
     new GameObject(
       new Vector(Math.random() * (canvas.width - IMAGE_SIZE), 0),
       new Vector(0, 2),
@@ -103,5 +111,21 @@ var createDropObject = function() {
   }
 }
 var checkCollisionDropObject = function() {
+  Object.keys(objects).forEach(key => {
+    if (key === player.id) return
+    
+    // 当たり判定
+    if (player.pos.x <= objects[key].pos.x + IMAGE_SIZE // 左端の判定
+      && player.pos.x + IMAGE_SIZE >= objects[key].pos.x // 右端の判定
+      && player.pos.y <= objects[key].pos.y + IMAGE_SIZE) { // 上端の判定
+      objects[key].destroy()
+      score += SCORE_RISE_POINT
+    }
+  })
+}
 
+var debugConsole = function() {
+  if (playTime % 400 == 0) {
+    console.log(objects)
+  }
 }
